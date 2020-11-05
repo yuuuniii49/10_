72f99d95f6e26530662c493efeefefc1d3ecda01
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-05 16:06:51 +0900

    04

--- main.c
+++ main.c
@@ -3,21 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void swap(int *x,int*y){
+	
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 int main(int argc, char *argv[]) {
 	
-	int i = 10;
-	char c = 'a';;
-	
-	int *iptr;
-	iptr = &i;
-	char *cptr;
-	cptr = &c;
-	int *iptr2;
-	iptr2 = iptr;
-	
-	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
-	
+	int a = 3;
+	int b = 5;
+	swap(&a,&b);
+	printf("a:%i,b:%i\n",a,b);
 	return 0;
 }
