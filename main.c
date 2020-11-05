853b71bffd28bb6771c268e6416fd269e7613a15
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-05 15:49:26 +0900

    02

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 int main(int argc, char *argv[]) {
 	
 	int i = 10;
-	char c = 69;
-	float f = 12.3;
+	char c = 'a';;
 	
-	printf("i : %i %p\n", i, &i);
-	printf("c : %c %p\n", c, &c);
-	printf("f : %f %p\n", f, &f);
+	int *iptr;
+	iptr = &i;
+	char *cptr;
+	cptr = &c;
+	int *iptr2;
+	iptr2 = iptr;
+	
+	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
+	
 	return 0;
 }
